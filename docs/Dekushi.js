51fe2b18bf918396c951c1e3b19048ad92e6173e
Dekushi <51485860+Dekushi@users.noreply.github.com>
2019-06-08 04:10:23 +0900

    地图

--- docs/Dekushi.js
+++ docs/Dekushi.js
@@ -16,13 +16,7 @@ function SetDivHidden(){
 }
 
 function mkMap(){
-    var mapType = google.maps.MapTypeId.ROADMAP;
-    var lat = 35.939759, lng = 139.435767, zoom = 3;
-    var mapOptions = {
-        center: new google.maps.LatLng(lat, lng),  //地图的中心点
-        zoom: zoom,               　　　　　　　　　　//地图缩放比例
-        mapTypeId: mapType,       　　　　　　　　　　//指定地图展示类型：卫星图像、普通道路
-        scrollwheel: true          　　　　　　　　　 //是否允许滚轮滑动进行缩放
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions); //创建谷歌地图
+    var lat = 35.9396259, lng = 139.4352337, zoom = 19;
+    var ymap = new Y.Map("map");
+    ymap.drawMap(new Y.LatLng(lat, lng), zoom, Y.LayerSetId.NORMAL);
 }
