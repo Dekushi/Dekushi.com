8042ef3fb48124afed932574bc13324a3483fb8f
Dekushi <51485860+Dekushi@users.noreply.github.com>
2019-06-08 02:56:59 +0900

    地图

--- docs/Dekushi.js
+++ docs/Dekushi.js
@@ -1,15 +1,28 @@
 function TagClick(Type){
     SetDivHidden();
-    var objDiv 
-    if(Type == 1){
-        objDiv = document.getElementById("div1");
-    }else if(Type == 2){
-        objDiv = document.getElementById("div2");
+    var DivID = "div" + Type;
+    if(Type == 4){
+        mkMap();
     }
+    var objDiv = document.getElementById(DivID);
     objDiv.style.display = "block";
 }
 
 function SetDivHidden(){
     document.getElementById("div1").style.display = "none";
     document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+}
+
+function mkMap(){
+    var mapType = google.maps.MapTypeId.ROADMAP;
+    var lat = 35.939759, lng = 139.435767, zoom = 3;
+    var mapOptions = {
+        center: new google.maps.LatLng(lat, lng),  //地图的中心点
+        zoom: zoom,               　　　　　　　　　　//地图缩放比例
+        mapTypeId: mapType,       　　　　　　　　　　//指定地图展示类型：卫星图像、普通道路
+        scrollwheel: true          　　　　　　　　　 //是否允许滚轮滑动进行缩放
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions); //创建谷歌地图
 }
